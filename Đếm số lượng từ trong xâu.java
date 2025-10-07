d5daaa3a7ff5562144dc65e7a8b1624ba89f7325
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-07 08:25:24 +0700

    Create Đếm số lượng từ trong xâu.java

---
+++ Đếm số lượng từ trong xâu.java
@@ -0,0 +1,17 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String s = sc.nextLine();
+        String[] a = s.split("\\s+");
+        System.out.print(a.length);
+    }
+}
