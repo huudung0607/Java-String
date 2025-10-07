3b4c6cce8b56b2239d5b9aa4b23fdf03deca52f4
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-07 08:08:01 +0700

    Create In hoa, in thường.java

---
+++ In hoa, in thường.java
@@ -0,0 +1,19 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String s = sc.nextLine();
+        s = s.toUpperCase();
+        System.out.println(s);
+        s = s.toLowerCase();
+        System.out.print(s);
+    }
+}
